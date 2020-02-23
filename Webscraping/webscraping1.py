17b0288023ed3b635711dfd8487962d4d3ce3e02
Aman3786 <53119534+Aman3786@users.noreply.github.com>
2020-02-23 11:59:24 +0530

    Add files via upload

---
+++ Webscraping/webscraping1.py
@@ -0,0 +1,35 @@
+import requests
+from requests_html import HTMLSession
+import csv
+ 
+session =HTMLSession()
+r =session.get("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
+
+print(r)
+
+lister =r.html.find(".titleColumn")
+
+rate =r.html.find(".imdbRating strong")
+
+
+
+image = r.html.find(".posterColumn a img")
+
+for i in range(0,len(lister)):
+    print(lister[i].text)
+    print(rate[i].text)
+    print(image[i].attrs["src"])
+    print("")
+
+header = ["Title","Rating","Poster"]
+data =open("imdb.csv","w")
+writer =csv.writer(data)
+writer.writerow(header)
+
+for i in range(0,len(lister)):
+    a =[lister[i].text,rate[i].text,image[i].attrs["src"]]
+    writer.writerow(a)
+    
+
+
+    
